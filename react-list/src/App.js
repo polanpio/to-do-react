9fe5ae3fccaff44f230053b7411c35cb46bd578e
Piotr Polanski <polanski.pio@gmail.com>
2021-09-28 22:10:18 +0900

    first components

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+import Form from './components/Form'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>React's Todo List</h1>
+      </header>
+      < Form />
     </div>
   );
 }
